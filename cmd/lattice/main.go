c122281ae0ddabb28f8633c3fb312a5b42c4db28
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2026-07-30 12:49:09 +0530

    added indexer

--- cmd/lattice/main.go
+++ cmd/lattice/main.go
@@ -2,40 +2,36 @@ package main
 
 import (
 	"Lattice/internal/parser"
-	"Lattice/internal/repository"
 	"fmt"
 	"log"
 )
 
 func main() {
 	log.Println("Lattice is running")
-	log.Println("...")
+
 	var root string
 	fmt.Scanf("%s", &root)
-	files, err := repository.Walk(root)
+
+	index, err := parser.BuildRepositoryIndex(root)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, file := range files {
-		log.Println("Parsing:", file)
 
-		parsedFile, err := parser.ParseFile(file)
-		if err != nil {
-			log.Fatal(err)
-		}
+	for _, file := range index.Files {
+		log.Println("Parsing:", file.Path)
 
 		log.Println("Symbols:")
-		for _, symbol := range parsedFile.Symbols {
+		for _, symbol := range file.Symbols {
 			log.Println(symbol.Name, symbol.Kind)
 		}
 
 		log.Println("Imports:")
-		for _, imp := range parsedFile.Imports {
+		for _, imp := range file.Imports {
 			log.Println(imp.Path)
 		}
 
 		log.Println("Calls:")
-		for _, call := range parsedFile.Calls {
+		for _, call := range file.Calls {
 			log.Printf("%s.%s", call.Receiver, call.Target)
 		}
 	}
---
+++ internal/parser/indexer.go
@@ -0,0 +1,23 @@
+package parser
+
+import "Lattice/internal/repository"
+
+func BuildRepositoryIndex(path string) (*RepositoryIndex, error) {
+	files, err := repository.Walk(path)
+	if err != nil {
+		return nil, err
+
+	}
+	index := &RepositoryIndex{}
+
+	for _, file := range files {
+		parsedFile, err := ParseFile(file)
+		if err != nil {
+			return nil, err
+		}
+
+		index.Files = append(index.Files, parsedFile)
+	}
+	return index, nil
+
+}
